952e806a5c180daec67d34a65fb844c735e720e9
Vladimir Stoyanov <69723159+VladoDev03@users.noreply.github.com>
2025-12-06 03:38:38 +0200

    Added rocket movement.

---
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private InputAction thrust;
+    [SerializeField] private InputAction rotation;
+    [SerializeField] private float thrustStrength = 100f;
+    [SerializeField] private float rotationStrength = 100f;
+    
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        RigidbodyConstraints constraints = rb.constraints;
+
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+
+        rb.constraints = constraints;
+    }
+}
